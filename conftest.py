94b7e691883c96b9742d558fd2fead4591c4469b
Ilana Cabral <ilana.cabral@gmail.com>
2022-04-28 08:59:15 -0300

    Including database isolation

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
-from os import sched_param
-from tempfile import tempdir
 import pytest
 from unittest.mock import patch
 from sqlmodel import create_engine
 from beerlog import models
 
-@pytest.fixture(autouse=True,scope="function")
-def each_uses_separete_database(request):
+
+@pytest.fixture(autouse=True, scope="function")
+def each_test_uses_separate_database(request):
     tmpdir = request.getfixturevalue("tmpdir")
+    test_db = tmpdir.join("beerlog.test.db")
+    engine = create_engine(f"sqlite:///{test_db}")
+    models.SQLModel.metadata.create_all(bind=engine)
+    with patch("beerlog.database.engine", engine):
+        yield
     
